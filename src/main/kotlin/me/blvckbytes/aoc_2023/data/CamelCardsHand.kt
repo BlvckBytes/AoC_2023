f468a68b724b28d5b9596e681c13435a5cf41efd
Philip Hell <blvckbytes@gmail.com>
2023-12-07 17:05:15 +0100

    massively simplified day 7's logic

--- src/main/kotlin/me/blvckbytes/aoc_2023/data/CamelCardsHand.kt
+++ src/main/kotlin/me/blvckbytes/aoc_2023/data/CamelCardsHand.kt
@@ -12,114 +12,24 @@ class CamelCardsHand(
     if (cards.size != 5)
       throw IllegalStateException("A hand has to consist of five cards exactly")
 
-    val occurrences = IntArray(CamelCard.strengthSize) { 0 }
+    val frequencies = IntArray(CamelCard.strengthSize) { 0 }
 
     for (card in cards)
-      ++occurrences[card.strength]
+      ++frequencies[card.strength]
 
-    var minOccurrence: Int? = null
-    var maxOccurrence: Int? = null
+    var numberOfJokers = 0
 
-    for (occurrence in occurrences) {
-      if (occurrence == 0)
-        continue
-
-      if (minOccurrence == null || occurrence < minOccurrence)
-        minOccurrence = occurrence
-
-      if (maxOccurrence == null || occurrence > maxOccurrence)
-        maxOccurrence = occurrence
+    if (withJBeingJoker) {
+      numberOfJokers = frequencies[CamelCard.jokerIndex]
+      frequencies[CamelCard.jokerIndex] = 0
     }
 
-    if (minOccurrence == null || maxOccurrence == null)
-      throw IllegalStateException("Could not determine min/max occurrences")
+    frequencies.sortDescending()
 
-    // J cards can pretend to be whatever card is best for the purpose of determining hand type
-    val numberOfJokers = (
-      if (withJBeingJoker)
-        occurrences[CamelCard.jokerIndex]
-      else
-        0
-    )
+    if (withJBeingJoker)
+      frequencies[0] += numberOfJokers
 
-    // all five cards have the same label
-    // XXXXX
-    if (maxOccurrence == 5)
-      type = CamelCardsHandType.FIVE_OF_A_KIND
-
-    // four cards have the same label and one card has a different label
-    // XXXXY
-    else if (maxOccurrence == 4) {
-      type = when(numberOfJokers) {
-        // JJJJY -> YYYYY, XXXXJ -> XXXXX
-        4, 1 -> CamelCardsHandType.FIVE_OF_A_KIND
-        0 -> CamelCardsHandType.FOUR_OF_A_KIND
-        else -> throw IllegalStateException("Illegal number of jokers $numberOfJokers")
-      }
-    }
-
-    // three cards have the same label, and the remaining two cards share a different label
-    // XXXYY
-    else if (maxOccurrence == 3 && minOccurrence == 2) {
-      type = when (numberOfJokers) {
-        // JJJYY -> YYYYY, XXXJJ -> XXXXX
-        3, 2 -> CamelCardsHandType.FIVE_OF_A_KIND
-        1 -> throw IllegalStateException("Impossible case")
-        0 -> CamelCardsHandType.FULL_HOUSE
-        else -> throw IllegalStateException("Illegal number of jokers $numberOfJokers")
-      }
-    }
-
-    // three cards have the same label, and the remaining two cards are each different from any other card in the hand
-    // XXXYZ
-    else if (maxOccurrence == 3 && minOccurrence == 1) {
-      type = when (numberOfJokers) {
-        2 -> throw IllegalStateException("Impossible case")
-        // JJJYZ -> XXXXZ, XXXJZ -> XXXXZ
-        3, 1 -> CamelCardsHandType.FOUR_OF_A_KIND
-        0 -> CamelCardsHandType.THREE_OF_A_KIND
-        else -> throw IllegalStateException("Illegal number of jokers $numberOfJokers")
-      }
-    }
-
-    // two cards share one label, two other cards share a second label, and the remaining card has a third label
-    // XXYYZ
-    else if (occurrences.count { it == 2 } == 2) {
-      type = when (numberOfJokers) {
-        // XXJJZ -> XXXXZ
-        2 -> CamelCardsHandType.FOUR_OF_A_KIND
-        // XXYYJ -> XXYYY
-        1 -> CamelCardsHandType.FULL_HOUSE
-        0 -> CamelCardsHandType.TWO_PAIR
-        else -> throw IllegalStateException("Illegal number of jokers $numberOfJokers")
-      }
-    }
-
-    // two cards share one label, and the other three cards have a different label from the pair and each other
-    // XXYZW
-    else if (maxOccurrence == 2 && occurrences.count { it == 1 } == 3) {
-      type = when(numberOfJokers) {
-        // JJYZW -> YYYZW, XXJZW -> XXXZW
-        2, 1 -> CamelCardsHandType.THREE_OF_A_KIND
-        0 -> CamelCardsHandType.ONE_PAIR
-        else -> throw IllegalStateException("Illegal number of jokers $numberOfJokers")
-      }
-    }
-
-    // all cards' labels are distinct
-    // VWXYZ
-    else if (maxOccurrence == 1) {
-      type = when (numberOfJokers) {
-        5, 4, 3, 2 -> throw IllegalStateException("Impossible case")
-        // VJXYZ -> VVXYZ
-        1 -> CamelCardsHandType.ONE_PAIR
-        0 -> CamelCardsHandType.HIGH_CARD
-        else -> throw IllegalStateException("Illegal number of jokers $numberOfJokers")
-      }
-    }
-
-    else
-      throw IllegalStateException("Could not decide on a hand type")
+    type = CamelCardsHandType.findMatching(frequencies)
   }
 
   override fun compareTo(other: CamelCardsHand): Int {
--- src/main/kotlin/me/blvckbytes/aoc_2023/data/CamelCardsHandType.kt
+++ src/main/kotlin/me/blvckbytes/aoc_2023/data/CamelCardsHandType.kt
@@ -1,11 +1,51 @@
 package me.blvckbytes.aoc_2023.data
 
-enum class CamelCardsHandType {
-  FIVE_OF_A_KIND,
-  FOUR_OF_A_KIND,
-  FULL_HOUSE,
-  THREE_OF_A_KIND,
-  TWO_PAIR,
-  ONE_PAIR,
-  HIGH_CARD,
+enum class CamelCardsHandType(
+  private val frequencies: IntArray,
+  private val numberOfFrequencies: Int,
+) {
+  FIVE_OF_A_KIND(5),
+  FOUR_OF_A_KIND(4, 1),
+  FULL_HOUSE(3, 2),
+  THREE_OF_A_KIND(3, 1, 1),
+  TWO_PAIR(2, 2, 1),
+  ONE_PAIR(2, 1, 1, 1),
+  HIGH_CARD(1, 1, 1, 1, 1),
+  ;
+
+  constructor(vararg frequencies: Int) : this(frequencies, frequencies.size)
+
+  companion object {
+    private val values = values()
+
+    fun findMatching(sortedFrequencies: IntArray): CamelCardsHandType {
+      for (value in values) {
+        if (value.matches(sortedFrequencies))
+          return value
+      }
+
+      throw IllegalStateException("Could not find a match for the frequencies $sortedFrequencies")
+    }
+  }
+
+  fun matches(sortedFrequencies: IntArray): Boolean {
+    if (sortedFrequencies.size < numberOfFrequencies)
+      return false
+
+    for (frequencyIndex in sortedFrequencies.indices) {
+      val frequency = sortedFrequencies[frequencyIndex]
+
+      if (frequencyIndex >= numberOfFrequencies) {
+        if (frequency != 0)
+          return false
+
+        continue
+      }
+
+      if (frequency != frequencies[frequencyIndex])
+        return false
+    }
+
+    return true
+  }
 }
