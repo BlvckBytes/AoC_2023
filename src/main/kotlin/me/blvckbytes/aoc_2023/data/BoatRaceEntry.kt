822f7f8a3272f45c1a65241f0820dcd1229280be
Philip Hell <blvckbytes@gmail.com>
2023-12-07 11:37:28 +0100

    improved the efficiency of day 6 by migrating from a counting loop to constant time evaluation

--- src/main/kotlin/me/blvckbytes/aoc_2023/data/BoatRaceEntry.kt
+++ src/main/kotlin/me/blvckbytes/aoc_2023/data/BoatRaceEntry.kt
@@ -1,25 +1,71 @@
 package me.blvckbytes.aoc_2023.data
 
+import kotlin.math.ceil
+import kotlin.math.floor
+import kotlin.math.sqrt
+
 data class BoatRaceEntry(
   val time: Long,
   val distance: Long,
 ) {
 
   fun countNumberOfWaysToWin(raceEntry: BoatRaceEntry): Long {
-    val currentBestDistance = raceEntry.distance
-    var result = 0L
+    /*
+      distance(chargeTime) = (raceTime - chargeTime) * chargeTime
+      distance(chargeTime) = -chargeTime^2 + raceTime * chargeTime
+      distance'(chargeTime) = -2 * chargeTime + raceTime
 
-    // 0 and time don't make any sense, as with zero there's no speed
-    // and with time there's no time left to move, no matter how fast
-    for (chargeTime in 1 until raceEntry.time) {
-      val raceTime = raceEntry.time - chargeTime
-      val distanceTravelled = chargeTime * raceTime // [mm/ms * ms]
+      0 = -2 * chargeTime + raceTime
+      2 * chargeTime = raceTime
+      chargeTime = raceTime / 2
 
-      if (distanceTravelled > currentBestDistance)
-        ++result
-    }
+      => distance(raceTime / 2) is the max. distance possible
 
-    return result
+      distance = -chargeTime^2 + raceTime * chargeTime
+      0 = -chargeTime^2 + raceTime * chargeTime - distance
+      0 = chargeTime^2 - raceTime * chargeTime + distance
+
+      chargeTime = -(-raceTime/2) +- sqrt((-raceTime/2)^2 - distance)
+      chargeTime = raceTime/2 +- sqrt(raceTime^2/4 - distance)
+      chargeTime = raceTime/2 +- sqrt(raceTime^2/4 - (4*distance)/4)
+      chargeTime = raceTime/2 +- sqrt((raceTime^2 - 4*distance)/4)
+
+      => chargeTime(distance) = raceTime/2 +- sqrt(raceTime^2 - 4*distance)/2
+     */
+
+    val raceTimeHalf = raceEntry.time / 2.0
+    val currentBestChargeTime = raceTimeHalf - sqrt(raceEntry.time * raceEntry.time - 4.0 * raceEntry.distance) / 2.0
+
+    if (currentBestChargeTime == raceTimeHalf)
+      return 0
+
+    val currentBestInFirstHalf = currentBestChargeTime < raceTimeHalf
+
+    val nextBestChargeTime = (
+      // Original choice was not aligned to the whole number grid, thus the next whole choice is a valid choice
+      if (currentBestChargeTime % 1.0 != 0.0) {
+        if (currentBestInFirstHalf)
+          ceil(currentBestChargeTime)
+        else
+          floor(currentBestChargeTime)
+      }
+
+      // Was aligned to the whole number grid, advance to next choice
+      else {
+        if (currentBestInFirstHalf)
+          currentBestChargeTime + 1
+        else
+          currentBestChargeTime - 1
+      }
+    )
+
+    // * 2 for both sides of the parabola (slow speed, much time == high speed, little time)
+    // + 1 for the equal choice skipped by 2*(floor/ceil/+1/-1) on the un-chosen other half
+    return ((
+      if (currentBestInFirstHalf)
+        (raceTimeHalf - nextBestChargeTime)
+      else
+        (nextBestChargeTime - raceTimeHalf)
+    ) * 2 + 1).toLong()
   }
-
 }
